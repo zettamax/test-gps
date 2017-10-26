646c994448c8d31a1fddc7f71df6e3ab6e81d5be
Max Nazarov <zettamax@gmail.com>
2017-10-26 19:30:14 +0300

    On update position handler

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 var x = document.querySelector("#demo");
+var geoOptions = {
+    maximumAge: 10000,
+    enableHighAccuracy: true
+};
+var j = JSON.stringify;
+
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.watchPosition(function (p) {
+            showPosition(p, '0a0');
+        }, errorPosition, geoOptions);
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude; 
+function showPosition(position, color) {
+
+    var coords = j({
+        lat: position.coords.latitude,
+        lon: position.coords.longitude
+    });
+
+    if (x.dataset.coords === coords) {
+        return;
+    }
+    x.dataset.coords = coords;
+
+    x.innerHTML +=
+        "<div class='loc' style='position: absolute;'>" +
+        "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude + '</div>';
+
+    var loc = document.querySelector('.loc:not(:first-child)');
+    x.removeChild(loc);
+}
+function errorPosition(e) {
+    console.log(e);
 }
