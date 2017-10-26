6dcf1d6aa56e6383181864fffa958a6b7899479d
Max Nazarov <zettamax@gmail.com>
2017-10-27 01:10:52 +0300

    Magic

--- sw.js
+++ sw.js
@@ -2,4 +2,23 @@ self.addEventListener('fetch', function (event) {
     if (event.request.url.startsWith(self.location.origin)) {
         event.respondWith(fetch(event.request));
     }
+
+    var title = 'Yay a message.';
+    var body = event.request.url;
+    var icon = './icon128.png';
+    var tag = 'ololo-tag';
+
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body: body,
+            icon: icon,
+            tag: tag
+        })
+    );
+});
+
+self.addEventListener('push', function(event) {
+    console.log('Received a push message', event);
+
+
 });
